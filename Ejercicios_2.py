f4c2892cdea935d0a4300e84c9de34a92fb3726c
Ignacio Cavallo <40524472+cavigna@users.noreply.github.com>
2022-05-22 23:45:55 -0400

    Added Exc 2

---
+++ Ejercicios_2.py
@@ -0,0 +1,36 @@
+# Borrar todos los números 31 de un listado
+
+
+treinta_uno = [23, 31,6, 31, 95, 4, 31, 2, 33, 3, 31]
+
+
+# Encontrar el valor máximo de un listado de numeros. NO USAR max()
+# Encontrar el valor mínimo de un listado de numeros. NO USAR min()
+
+maximo_minimo = [7, 93, 2, 8, 425, 1099901, 9, 1101000193, 393934, 3]
+
+
+#Encontrar dentro de una string, aquella palabra con la mayor cantidad de letras.
+
+lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis facilisis molestie. In sed elit ut mauris iaculis porttitor. Vestibulum ac justo lobortis, rhoncus eros sed, feugiat ante. Nunc interdum, nunc at pellentesque facilisis, urna libero venenatis purus, sed dignissim orci diam id urna. Aenean placerat leo eget felis efficitur."
+
+
+#Encontrar los calores repetidos en una lista y mostrarlos en una nueva lista
+
+repetidos = [12, 23, 4, 6, 22,99,37, 52, 12, 65, 99, 33, 37]
+
+unicos = []
+
+
+#Dada una lista, imprimir la suma de los números pares e imapres.
+#Consejo: Ver en el repo, el uso de break
+
+listado_pares_e_impares= [22, 32, 42, 13, 35, 13, 6, 42, 13, 25, 28, 15, 14, 
+30, 42, 32, 46, 0, 47, 46]
+
+
+
+
+
+
+
